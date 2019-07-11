69590d9b5605a420e8143e8287d7c9128ffeaa60
coLlenz <collenzaligway@gmail.com>
2019-07-12 01:31:19 +0800

    "USERS API"

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+let url = 'https://jsonplaceholder.typicode.com/users';
+
+
+const fetchCountry = async () => {
+    return await fetch(url)
+    .then(res => res.json())
+    .then((data) => {
+        let userDatas = '';
+        data.map((users) => {
+            userDatas += `
+                <tr>
+                    <td>${users.id}</td>
+                    <td>${users.name}</td>
+                    <td>${users.username}</td>
+                    <td>${users.email}</td>
+                </tr>
+            `
+        })  
+        document.querySelector('#userDataTable').innerHTML = userDatas
+    })
+}
+
+fetchCountry();
+
+
+
